0e6b4115406cd5f8d95f5cca123eae77d4e8eed7
Dylan Tobia <djtobia@gmail.com>
2020-12-15 23:01:48 -0800

    trying my own solution to day10

--- day10.js
+++ day10.js
@@ -26,6 +26,77 @@ console.log("Part 1 :", oneDiffs * threeDiffs);
 input.unshift(0);
 
 input.push(input[input.length - 1] + 3);
+//build graph
+class Node {
+  value = "";
+  children = [];
+  visited = false;
+
+  constructor(val) {
+    this.value = val;
+  }
+
+  addChild(childIndex) {
+    this.children.push(childIndex);
+  }
+  getChildren() {
+    return this.children;
+  }
+  toString() {
+    return this.val;
+  }
+  getValue() {
+    return this.value;
+  }
+  setVisited(value) {
+    this.visited = value;
+  }
+  isVisited() {
+    return this.visited;
+  }
+}
+
+//loop over items in input to build graph
+let list = [];
+for (let i = 0; i < input.length; i++) {
+  //create array of node objects
+  list.push(new Node(input[i]));
+}
+//now iterate over list, adding child pointers
+for (let i = 0; i < input.length; i++) {
+  for (let j = i + 1; j < i + 4 && j < input.length; j++) {
+    if (list[j].getValue() < list[i].getValue() + 4) {
+      list[i].addChild(j);
+    }
+  }
+}
+
+let pathCount = 0;
+function DFS(list, index) {
+  const path = [];
+  path.push(index);
+  while (path.length) {
+    let index = path.pop();
+    if (!list[index].isVisited()) {
+      list[index].setVisited(true);
+    }
+    if (list[index].getValue() === list[list.length - 1].getValue()) {
+      pathCount++;
+    } else {
+      for (let i = 0; i < list[index].getChildren().length; i++) {
+        if (!list[list[index].getChildren()[i]].isVisited()) {
+          path.push(list[index].getChildren()[i]);
+        }
+      }
+    }
+
+    // path.pop();
+    list[index].setVisited(false);
+  }
+}
+DFS(list, 0);
+console.log("Part 2:", pathCount);
+console.log("Should be:", 56693912375296);
 let pow7 = 0;
 let pow2 = 0;
 for (let i = 1; i < input.length - 1; i++) {
